9622d8acb33555684c86448551503f13d0139365
Jang <87692888+JangSeongwon@users.noreply.github.com>
2022-05-25 11:08:09 +0900

    Update panda_grasping.py

--- src/environments/panda_grasping.py
+++ src/environments/panda_grasping.py
@@ -268,7 +268,7 @@ class Pandagrasping(PandaEnv):
 
         state["object-state"] = np.concatenate([cube_pos, cube_quat, di["gripper_to_cube"]])
 
-        """2개만 랜덤으로 모양나오게 """
+        """ 랜덤으로 속도형성,  """
         self.sim.data.qvel[9:12] = np.random.randn(3)*(0.5,1,0.5)
         self.sim.data.qvel[12:16] = np.random.randn(3)*0.1
 
