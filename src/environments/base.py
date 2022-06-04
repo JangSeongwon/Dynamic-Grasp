4d85316f02b0f6ccf3933528b1d0e0558b7fb39e
Jang <87692888+JangSeongwon@users.noreply.github.com>
2022-06-04 21:01:32 +0900

    Update base.py

--- src/environments/base.py
+++ src/environments/base.py
@@ -17,14 +17,9 @@ def register_env(target_class):
     REGISTERED_ENVS[target_class.__name__] = target_class
 
 def get_env(name):
-    """Try to get the equivalent functionality of gym.make in a sloppy way."""
-
     return REGISTERED_ENVS[name]
 
 def make(name, config=None):
-    """
-    Creates a new environment instance with @name and @config.
-    """
     env = get_env(name)
 
     # get default configuration
@@ -40,7 +35,6 @@ def make(name, config=None):
     return env(config)
 
 class EnvMeta(type):
-    """Metaclass for registering environments"""
 
     def __new__(meta, name, bases, class_dict):
         cls = super().__new__(meta, name, bases, class_dict)
@@ -52,10 +46,7 @@ class EnvMeta(type):
             register_env(cls)
         return cls
 
-
 class MujocoEnv(metaclass=EnvMeta):
-    """Initializes a Mujoco Environment."""
-
     def __init__(self, config):
 
         self.render_collision_mesh = config.render_collision_mesh
@@ -76,31 +67,18 @@ class MujocoEnv(metaclass=EnvMeta):
         self._reset_internal()
 
     def initialize_time(self, control_freq):
-        """
-        Initializes the time constants used for simulation.
-        """
         self.cur_time = 0
         self.model_timestep = self.sim.model.opt.timestep #0.002 is default simulation timestep in mujoco
         self.control_freq = control_freq
         self.control_timestep = 1. / control_freq # 0.01 if control freq is 100
 
     def _load_model(self):
-        """Loads an xml model, puts it in self.model"""
         pass
 
     def _get_reference(self):
-        """
-        Sets up references to important components. A reference is typically an
-        index or a list of indices that point to the corresponding elements
-        in a flatten array, which is how MuJoCo stores physical simulation data.
-        """
         pass
 
     def _initialize_sim(self, xml_string=None):
-        """
-        Creates a MjSim object and stores it in self.sim. If @xml_string is specified, the MjSim object will be created
-        from the specified xml_string. Else, it will pull from self.model to instantiate the simulation
-        """
         # if we have an xml string, use that to create the sim. Otherwise, use the local model
         self.mjpy_model = self.model.get_model()
 
@@ -112,13 +90,11 @@ class MujocoEnv(metaclass=EnvMeta):
         self.initialize_time(self.control_freq)
 
     def reset(self):
-        """Resets simulation."""
         self._reset_internal()
         self.sim.forward()
         return self._get_observation()
 
     def _reset_internal(self):
-        """Resets simulation internal configurations."""
 
         # create visualization screen or renderer
         if self.viewer is None:
@@ -139,11 +115,9 @@ class MujocoEnv(metaclass=EnvMeta):
         self.done = False
 
     def _get_observation(self):
-        """Returns an OrderedDict containing observations [(name_string, np.array), ...]."""
         return OrderedDict()
 
     def step(self, action):
-        """Takes a step in simulation with control command @action."""
         if self.done:
             raise ValueError("executing action in terminated episode")
 
@@ -173,11 +147,9 @@ class MujocoEnv(metaclass=EnvMeta):
         return self._get_observation(), reward, done, info
 
     def _pre_action(self, action, policy_step=False):
-        """Do any preprocessing before taking an action."""
         self.sim.data.ctrl[:] = action #sim.data.ctrl is the control signal that is sent to the actuators in the simulation
 
     def _post_action(self, action):
-        """Do any housekeeping after taking an action."""
         reward = self.reward(action)
         info = {}
         info['episode_success'] = int(self._check_success())
@@ -189,17 +161,10 @@ class MujocoEnv(metaclass=EnvMeta):
         return reward, self.done, info
 
     def reward(self, action):
-        """Reward should be a function of state and action."""
         return 0
 
     def render(self):
-        """
-        Renders to an on-screen window.a
-        """
         self.viewer.render()
 
     def _check_success(self):
-        """
-        Returns True if task has been completed.
-        """
         return False
